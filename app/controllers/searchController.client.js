aaf8dbd56a89e5d085a1b3e5438a260e2d5bc91d
Rich <ibanez19@sbcglobal.net>
2017-04-01 19:43:32 +0000

    Format search results

--- app/controllers/searchController.client.js
+++ app/controllers/searchController.client.js
@@ -2,15 +2,34 @@
 /* global $, FB, localStorage, ajaxFunctions */
 'use strict';
 
-const searchBtn = document.querySelector('#searchBtn');
 const apiUrl = '/api/list';
 
 //Populate page with search results
 function updateResults(data) {
    let list = JSON.parse(data);
    console.log(list);
+   //Clear previous search results
+   $('#list').empty();
    list.forEach(e => {
-      $('#list').append(e.name+'<br>');
+      $('#list').append(`
+         <div class="col s12 m4">
+            <div class="card small black">
+               <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="${e.image_url}" alt="${e.name}">
+               </div>
+               <div class="card-content black">
+                  <span class="card-title activator white-text">${e.name}
+                  <i class="material-icons right">more_vert</i></span>
+                  <p><a href="#">I'm going</a></p>
+                </div>
+                <div class="card-reveal black">
+                  <span class="card-title white-text">${e.name}
+                  <i class="material-icons right">close</i></span>
+                  <p>${e.location.address1}<br>${e.location.city}, ${e.location.state} ${e.location.zip_code}</p>
+                </div>
+              </div>
+           </div>
+      `);
    });
 }
 
