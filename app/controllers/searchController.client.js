f51966f5bf85d22d17a6859d53b916f42e74f943
Rich <ibanez19@sbcglobal.net>
2017-04-02 18:37:34 +0000

    Animate and disable search button during AJAX call

--- app/controllers/searchController.client.js
+++ app/controllers/searchController.client.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const apiUrl = '/api/list';
+const $btn = $('#searchBtn');
 
 //Populate page with search results
 function updateResults(data) {
@@ -26,11 +27,15 @@ function updateResults(data) {
                   <span class="card-title white-text">${e.name}
                   <i class="material-icons right">close</i></span>
                   <p>${e.location.address1}<br>${e.location.city}, ${e.location.state} ${e.location.zip_code}</p>
+                  <p></p>
+                  <p><a class="yelp" href="${e.url}" target="_blank">View on Yelp <i class="fa fa-2x fa-yelp"></i></a></p>
                 </div>
               </div>
            </div>
       `);
    });
+   $btn.removeClass('disabled');
+	$btn.html('Search');
 }
 
 
@@ -39,12 +44,14 @@ function search(location) {
    //First, ensure search field is populated
    if (!location.trim()) return Materialize.toast('Please enter a valid location', 3000, 'error');
    //Then, perform search
+   $btn.addClass('disabled');
+	$btn.html('<i class="fa fa-spinner fa-spin fa-fw"></i>&nbsp;Searching');
    ajaxFunctions.ajaxRequest('GET', `${apiUrl}/${location}`, updateResults);
 }
 
 
 //Handle search button click...
-$('#searchBtn').click(() => search($('#locationInput').val()));
+$btn.click(() => search($('#locationInput').val()));
 
 //Handle enter-key submission from search field
 $('#locationForm').on('submit', e => {
