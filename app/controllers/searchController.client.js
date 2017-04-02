174843d3888eb0626fac5fbcb3a16b983db5ae83
Rich <ibanez19@sbcglobal.net>
2017-04-02 22:46:59 +0000

    Reformat search results to handle international addresses

--- app/controllers/searchController.client.js
+++ app/controllers/searchController.client.js
@@ -4,6 +4,7 @@
 
 const apiUrl = '/api/list';
 const $btn = $('#searchBtn');
+let lastSearch = '';
 
 //Populate page with search results
 function updateResults(data) {
@@ -12,8 +13,10 @@ function updateResults(data) {
    //Clear previous search results
    $('#list').empty();
    list.forEach(e => {
+      //Convert and round distance (in meters) to miles
+      let distance = (e.distance * 0.000621371192).toFixed(1);
       $('#list').append(`
-         <div class="col m4 l3">
+         <div class="col m6 l3">
             <div class="card small black">
                <div class="card-image waves-effect waves-block waves-light">
                   <img class="activator" src="${e.image_url}" alt="${e.name}">
@@ -21,13 +24,15 @@ function updateResults(data) {
                <div class="card-content black">
                   <span class="card-title activator white-text">${e.name}
                   <i class="material-icons right">more_vert</i></span>
+                  <p>${distance} mi.</p>
                   <p><a href="#">I'm going</a></p>
                 </div>
                 <div class="card-reveal black">
                   <span class="card-title white-text">${e.name}
                   <i class="material-icons right">close</i></span>
-                  <p>${e.location.address1}<br>${e.location.city}, ${e.location.state} ${e.location.zip_code}</p>
-                  <p></p>
+                  <p>${e.location.display_address.join('<br>')}
+                     <br><a href="tel:${e.phone}" target="_blank">${e.display_phone}</a>
+                  </p>
                   <p><a class="yelp" href="${e.url}" target="_blank">View on Yelp <i class="fa fa-2x fa-yelp"></i></a></p>
                 </div>
               </div>
@@ -43,14 +48,17 @@ function updateResults(data) {
 function search(location) {
    //First, ensure search field is populated
    if (!location.trim()) return Materialize.toast('Please enter a valid location', 3000, 'error');
-   //Then, perform search
+   //Then, ensure user entered a new location (to prevent duplicate requests)
+   if (location.trim().toLowerCase() === lastSearch) return Materialize.toast('Please enter a new location', 3000, 'error');
+   //If a new valid location was entered, perform the search
    $btn.addClass('disabled');
 	$btn.html('<i class="fa fa-spinner fa-spin fa-fw"></i>&nbsp;Searching');
    ajaxFunctions.ajaxRequest('GET', `${apiUrl}/${location}`, updateResults);
+   lastSearch = location.trim().toLowerCase();
 }
 
 
-//Handle search button click...
+//Handle search button click
 $btn.click(() => search($('#locationInput').val()));
 
 //Handle enter-key submission from search field
