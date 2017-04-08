f26fa79d5e4df94640b59d8eb8dabd84a507ae12
Rich <ibanez19@sbcglobal.net>
2017-04-08 16:19:43 +0000

    Remove unused express-session and add favicon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const routes = require('./app/routes/index.js');
 const mongoose = require('mongoose');
-const session = require('express-session');
+const favicon = require('express-favicon');
 
 const app = express();
 require('dotenv').load();
@@ -11,19 +11,12 @@ require('dotenv').load();
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
+app.use(favicon(__dirname + '/public/favicon.png'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
 routes(app);
 
 const port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
+app.listen(port, () => console.log('Node.js listening on port ' + port + '...'));
