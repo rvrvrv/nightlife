d293ab0bac0c472b3d9c613a43b5e2f88332e7d1
rvrvrv <richvcode@gmail.com>
2018-10-01 11:16:40 -0500

    Refactor Yelp API call

--- src/controllers/yelpSearch.server.js
+++ src/controllers/yelpSearch.server.js
@@ -1,18 +1,16 @@
-'use strict';
-
 const yelp = require('yelp-fusion');
 
 function search(reqLocation, res) {
-  let searchRequest = {
+  const client = yelp.client(process.env.YELP_API_KEY);
+  const searchRequest = {
     term: 'nightlife',
     location: reqLocation
   };
-  yelp.accessToken(process.env.YELP_APP_ID, process.env.YELP_APP_SECRET).then(response => {
-    const client = yelp.client(response.jsonBody.access_token);
-    client.search(searchRequest).then(response => {
-      res.json(response.jsonBody.businesses);
-    });
-  }).catch(e => console.error(e));
+
+  client
+    .search(searchRequest)
+    .then(response => res.json(response.jsonBody.businesses))
+    .catch(e => console.error(e));
 }
 
 module.exports = search;
