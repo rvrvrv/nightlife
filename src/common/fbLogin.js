c7cac50c913394076e06838f7a5d3f56fc62586d
rvrvrv <richvcode@gmail.com>
2018-10-01 12:16:43 -0500

    Refactor client-side code w/ES6

--- src/common/fbLogin.js
+++ src/common/fbLogin.js
@@ -1,68 +1,59 @@
-/*jshint browser: true, esversion: 6*/
-/* global $, FB, localStorage, checkAll */
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: 1845310775708094,
+    cookie: true,
+    xfbml: true,
+    version: 'v2.9'
+  });
 
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: 1845310775708094,
-        cookie: true,
-        xfbml: true,
-        version: 'v2.9'
-    });
-
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-
+  FB.getLoginStatus(response => statusChangeCallback(response));
 };
 
-//Load the FB SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
+// Load the FB SDK asynchronously
+(function (d, s, id) {
+  const fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  const js = d.createElement(s);
+  js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 
-//Check for login status change
+// Check for login status change
 function statusChangeCallback(response) {
-    if (response.status === 'connected') loggedIn();
-    else loggedOut();
+  if (response.status === 'connected') loggedIn();
+  else loggedOut();
 }
 
-//Called after Login button is clicked
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
+// Called after login button is clicked
+function checkLoginState(reload) {
+  FB.getLoginStatus(res => statusChangeCallback(res, reload));
 }
 
-
-//Update page with logged-in user's info
+// Update page with logged-in user's info
 function loggedIn() {
-    FB.api('/me?fields=first_name, last_name, picture', function(user) {
-        localStorage.setItem('rv-nightlife-id', user.id);
-        $('#userInfo').html(`
-        <li><img class="valign left-align" src="${user.picture.data.url}" alt="${user.first_name} ${user.last_name}"></li>
-        <li class="hide-on-small-only">${user.first_name}</li>`);
-        $('#loginBtn').hide();
-        $('#logoutBtn').show();
-        $('#logoutLink').removeClass('hidden');
-        checkAll(); //Update attendance stats for visible locations
-    });
+  FB.api('/me?fields=first_name, last_name, picture', (user) => {
+    localStorage.setItem('rv-nightlife-id', user.id);
+    $('.user-info').html(`
+      <li><img class="valign left-align" src="${user.picture.data.url}" alt="${user.first_name} ${user.last_name}"></li>
+      <li class="hide-on-small-only">${user.first_name}</li>`);
+    $('#loginBtn').hide();
+    $('#logoutBtn').show();
+    $('#logoutLink').removeClass('hidden');
+    checkAll(); // Update attendance stats for visible locations
+  });
 }
 
-//Update page with logged-out view
+// Update page with logged-out view
 function loggedOut() {
-    localStorage.removeItem('rv-nightlife-id');
-    $('#userInfo').empty();
-    $('#logoutBtn').hide();
-    $('#loginBtn').show();
-    $('#logoutLink').addClass('hidden');
-    checkAll(); //Update attendance stats for visible locations
+  localStorage.removeItem('rv-nightlife-id');
+  $('.user-info').empty();
+  $('#logoutBtn').hide();
+  $('#loginBtn').show();
+  $('#logoutLink').addClass('hidden');
+  checkAll(); // Update attendance stats for visible locations
 }
 
-//Log out the user
+// Log out the user
 $('.logout-buttons').click(() => FB.logout(resp => checkLoginState()));
